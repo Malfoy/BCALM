b815bf93b7626f346afbe742d1c72958c106d9f7
Rayan Chikhi <chikhi@psu.edu>
2014-01-16 15:16:02 -0500

    minor fixes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <unordered_map>
 #include <algorithm>
+#include <cmath>
 #include "lm.h"
 #include "ograph.h"
 #include "debug.h"
@@ -57,7 +58,7 @@ int main(int argc, char ** argv)
 	int sys(0);
 	if(argc==1)
 	{
-        printf("usage: <input> [output.dot] [minimizer length] [kmer size]\n");
+        printf("usage: <input> [output.dot] [minimizer length]\n");
         printf("Note: default behavior (minimizer length = 10) requires that you type 'ulimit -n 1100' in your shell prior to running bcalm, else the software will crash\n");
         exit(1);
 	}
